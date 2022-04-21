71499b25c8f8f38a2e9008dac8f3134a07ea78ab
xanedesu <xanedesu@gmail.com>
2022-04-21 15:11:45 +0300

    Обновление

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post(':id/send')
+  @Post('sessions/:id/messages')
   send(@Param('id') id: string, @Body() message: any) {
     this.appService.sendMessage(id, message);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
   app.enableCors({
     origin: '*',
   });
 
-  await app.listen(3000);
+  await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
