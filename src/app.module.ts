efad754d4d49f6895e91209b546117aa7e97cecf
xanedesu <xanedesu@gmail.com>
2022-04-21 17:56:24 +0300

    Удалил serve static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import * as path from 'path';
 import { AppController } from './app.controller';
 import { AppGateway } from './app.gateway';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    ServeStaticModule.forRoot({
-      rootPath: path.resolve('public'),
-    }),
-  ],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppGateway, AppService],
 })
