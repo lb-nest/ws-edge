c785ea7effc23eef84bdd4b048159c82b79c45d6
xanedesu <xanedesu@gmail.com>
2022-06-21 16:12:17 +0300

    Валидация переменных окружения

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 import { SessionModule } from './session/session.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SessionModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(8080),
+        SECRET: Joi.string().required(),
+        MESSAGING_URL: Joi.string().uri().required(),
+      }),
+    }),
+    SessionModule,
+  ],
   controllers: [],
   providers: [],
 })
